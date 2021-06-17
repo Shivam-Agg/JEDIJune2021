516d768a30a5f9f19abbb4f13b6cb5dcd993f60e
Shivam <shivams14021999@gmail.com>
2021-06-17 10:11:58 +0530

    Changes committed: 	Added summation of four int arguments 	Refractored java code

--- CCode/addition.c
+++ CCode/addition.c
@@ -7,3 +7,8 @@ int sum(int x, int y){
 int sum3(int x, int y, int z){
   return x+y+z;
 }
+
+int sum4(int w, int x, int y, int z){
+  return w+x+y+z;
+}
+
--- JavaCode/Hello.java
+++ JavaCode/Hello.java
@@ -4,4 +4,7 @@ class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
     }
+    void hello(){
+    	System.out.println("Hello");
+    }
 }
